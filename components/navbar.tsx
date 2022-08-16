1f4b63d0cb2344c916b4dbd5a41b1b4bb0492472
Evan Bay <neopythe@protonmail.ch>
2022-08-16 17:17:00 -0600

    set sticky position on navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import AuthButton from './auth-button'
 
 export default function Component() {
   return (
-    <nav className="flex justify-end items-center h-20 border border-b-1 border-b-neutral-200 px-4">
+    <nav className="sticky w-full top-0 flex justify-end items-center h-20 border border-b-1 border-b-neutral-200 bg-white px-4">
       <AuthButton />
     </nav>
   )
