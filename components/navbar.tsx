fd7de952be13e444c7b1bd8dcf8344a4b489d731
Evan Bay <neopythe@protonmail.ch>
2022-08-16 15:22:54 -0600

    create navbar.tsx

---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+import LoginButton from './login-button'
+
+export default function Component() {
+  return (
+    <nav className="flex justify-end bg-blue-400">
+      <LoginButton />
+    </nav>
+  )
+}
