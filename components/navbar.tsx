af8710e580d82eb563b43ec78531d4b962339453
Evan Bay <neopythe@protonmail.ch>
2022-08-16 17:11:31 -0600

    update navbar.tsx classes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,9 @@
-import LoginButton from './login-button'
+import AuthButton from './auth-button'
 
 export default function Component() {
   return (
-    <nav className="flex justify-end bg-blue-400">
-      <LoginButton />
+    <nav className="flex justify-end items-center h-20 border border-b-1 border-b-neutral-200 px-4">
+      <AuthButton />
     </nav>
   )
 }
