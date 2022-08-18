879ccb6a938baf99e49605ce5e600be1d8948b29
Evan Bay <neopythe@protonmail.ch>
2022-08-17 21:47:37 -0600

    render posts data in index.tsx via getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import type { ReactElement } from 'react'
+import { useSession } from 'next-auth/react'
 import type { NextPageWithLayout } from './_app'
+
+import { prisma } from '../db'
+import { Post } from '@prisma/client'
 
 import Head from 'next/head'
 
@@ -7,7 +11,24 @@ import Layout from '../components/layout'
 
 import styles from '../styles/home.module.css'
 
-const Home: NextPageWithLayout = () => {
+export const getServerSideProps = async () => {
+  try {
+    const posts = await prisma.post.findMany()
+    return {
+      props: { posts },
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+interface HomeProps {
+  posts: Post[]
+}
+
+const Home: NextPageWithLayout<HomeProps> = ({ posts }: HomeProps) => {
+  const { data: session } = useSession()
+  console.log(posts)
   return (
     <div className={styles.container}>
       <Head>
@@ -16,87 +37,27 @@ const Home: NextPageWithLayout = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Next Blog</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        {!session && (
+          <p className={styles.description}>Sign in to get started</p>
+        )}
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <div key={post.id} className={styles.card}>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+            </div>
+          ))}
         </div>
       </main>
     </div>
   )
 }
 
-export default Home
-
 Home.getLayout = function getLayout(page: ReactElement) {
   return <Layout>{page}</Layout>
 }
+
+export default Home
